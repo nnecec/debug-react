adf29e43684115d40dd81954be0df8b11522b21c
nnecec <nnecec@outlook.com>
2022-05-13 14:58:38 +0800

    update diff

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # debug-react
+
+## Usage
+
+1. Open `browser devtool` and switch to `Sources` panel.
+2. Find the `react.development.js`: `top/localhost:3000/User/debug-react/node_modules/react-dom/cjs/react-dom.development.js`
+3. Search `function createRoot(container, options)`
+4. Start your React travel.
--- src/pages/test-diff/index.tsx
+++ src/pages/test-diff/index.tsx
@@ -1,44 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { getObserver } from "./util";
+import { useState } from "react";
 
 export const TestDiff = () => {
-  const [lastInput, setLastInput] = useState("");
-  const [input, setInput] = useState("");
+  const [eat, setEat] = useState(false);
 
-  useEffect(() => {
-    const diffObs = getObserver(".test-diff-seq");
-    diffObs();
-  }, []);
-
-  const patch = (e: any) => {
-    console.log(e);
-    setInput((last) => {
-      setLastInput(last);
-      return e;
-    });
-  };
-
-  const sequence = input.trim().split("");
+  const list = eat
+    ? ["apple", "orange", "banana", "watermelon"]
+    : ["apple", "banana", "watermelon"];
 
   return (
     <div>
-      <div>
-        <span color="gold">Inserted</span>
-        <span color="red">Moved</span>
-        <span color="green">Non moved</span>
-      </div>
-      <input placeholder="e.g. 123456" onChange={patch} />
-      <div>
-        {lastInput}
-        {"  ➔ "}
-        {input}
-      </div>
-      <hr />
-      <div className="test-diff-seq">
-        {sequence.map((it) => (
-          <span key={it}>{it}</span>
+      <blockquote>search: reconcileChildFibers</blockquote>
+      <h3>Fruit list {eat ? "eat" : ""}</h3>
+      <ul>
+        {list.map((text) => (
+          <li key={text}>{text}</li>
         ))}
-      </div>
+      </ul>
+
+      <button onClick={() => setEat(!eat)}>like what?</button>
     </div>
   );
 };
